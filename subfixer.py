a070e87f67738a81b4eede2bff4e78a840e43f47
Mad Ant <seedpuller@gmail.com>
2017-08-18 08:04:19 +0000

    Add new file

---
+++ subfixer.py
@@ -0,0 +1,9 @@
+import sys
+print("Subtitle Fixer Ver 0.1")
+args = sys.argv
+if len(args) < 1:
+    print('Usage : python subfixer.py "subtitle.srt"\n')
+presub = args[1]
+orgsub = open(presub, 'r', encoding='latin').read().encode('latin', errors='replace').decode('cp1256')
+open(str(presub).replace(".srt", "_fixed.srt"), 'w').write(orgsub)
+print('Subtitle has been successfully fixed . saved on : {0}'.format(str(presub).replace(".srt", "_fixed.srt")))
