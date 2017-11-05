ee968b98845e31390bcfca03051c805187ef6464
SeedPuller <seedpuller@gmail.com>
2017-11-05 06:48:54 +0000

    Update subfixer.py

--- subfixer.py
+++ subfixer.py
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
 #!/usr/bin/python3
 import sys
 print("Subtitle Fixer Ver 0.2")
-=======
-import sys
-print("Subtitle Fixer Ver 0.1")
->>>>>>> 375b16646f44eae8b262500bd7b2f20f23c3fc8f
 args = sys.argv
 if len(args) < 1:
     print('Usage : python subfixer.py "subtitle.srt"\n')
